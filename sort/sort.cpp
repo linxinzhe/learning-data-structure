e1f891bfbc8dff06a6ca546d2f9b8a9999daaad5
linxinzhe <linxinzhechn@icloud.com>
2018-02-19 13:15:54 +0800

    feat(sort): insertion optimize and nearly sort

--- sort/sort.cpp
+++ sort/sort.cpp
@@ -24,10 +24,32 @@ void insertionSort(T arr[], int n) {
         for (int j = i; j > 0; j--) {
             if (arr[j] < arr[j - 1]) {
                 swap(arr[j], arr[j - 1]);
-            }else{
+            } else {
                 break;
             }
         }
+    }
+}
+
+
+template<typename T>
+void insertionSort2(T arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        for (int j = i; j > 0 && arr[j] < arr[j - 1]; j--) {
+            swap(arr[j], arr[j - 1]);
+        }
+    }
+}
+
+template<typename T>
+void insertionSortOptimize(T arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        T e = arr[i];
+        int j;
+        for (j = i; j > 0 && arr[j - 1] > e; j--) {
+            arr[j] = arr[j - 1];
+        }
+        arr[j] = e;
     }
 }
 
@@ -35,9 +57,13 @@ int main() {
     int n = 10000;
     int *arr = SortTestHelper::generateRandomArray(n, 0, n);
     int *arr2 = SortTestHelper::copyIntArray(arr, n);
+    int *arr3 = SortTestHelper::copyIntArray(arr, n);
+    int *arr4 = SortTestHelper::generateNearlyOrderedArray(n, 100);
 
-    SortTestHelper::testSort("Insertion Sort", insertionSort, arr, n);
-    SortTestHelper::testSort("Selection Sort", selectionSort, arr, n);
+    SortTestHelper::testSort("InsertionOptimize Nearly Sort", insertionSortOptimize, arr4, n);
+    SortTestHelper::testSort("InsertionOptimize Sort", insertionSortOptimize, arr, n);
+    SortTestHelper::testSort("Insertion Sort", insertionSort, arr2, n);
+    SortTestHelper::testSort("Selection Sort", selectionSort, arr3, n);
 
     delete[] arr;
     return 0;
